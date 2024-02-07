571bc14fb8dd24ffe8a4f7f7705f39ad3057bb0c
Christian <mail@christianhohenbild.de>
2024-02-08 00:44:33 +0100

    add rudimentary home buttons

--- src/Blob.jsx
+++ src/Blob.jsx
@@ -1,19 +1,23 @@
 import { Canvas } from '@react-three/fiber'
+import { useNavigate } from 'react-router-dom'
 
 import './index.css'
 import Shader from './BlobShader.jsx'
 
 function App() {
   
+  let nav = useNavigate()
+
   return (
   <>
-
+     <button onClick={ (e) => {nav('/')} }>H o m e &rarr;</button>
     <Canvas
     camera={{ 
       position: [0, 0, 2],
       fov: 40 }}  
     >
       <color attach="background" args={[0x999999]} />
+     
       <Shader />
     </Canvas>
   </>
