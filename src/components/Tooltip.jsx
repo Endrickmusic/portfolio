6b99a1d287f339a86adfbf76716d9d6c1c6466b8
Christian Hohenbild <mail@christianhohenbild.de>
2024-12-22 18:55:26 +0100

    unify style of tags

--- src/components/Tooltip.jsx
+++ src/components/Tooltip.jsx
@@ -7,11 +7,15 @@ const Tooltip = ({ position, text }) => {
         position: "absolute",
         left: position.x,
         top: position.y,
-        background: "rgba(0, 0, 0, 0.7)",
-        color: "white",
-        padding: "5px",
-        borderRadius: "5px",
+        background: "white",
+        border: "1px solid black",
+        padding: "10px 20px",
+        color: "#000000",
+        borderRadius: "1px",
+        transform: "translate(0, -100%)",
+        whiteSpace: "nowrap",
         pointerEvents: "none",
+        zIndex: 1000,
       }}
     >
       {text}
