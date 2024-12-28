df8cfd5f30e6cd577ab5a0f563d0ab40ff01b8f7
Christian Hohenbild <mail@christianhohenbild.de>
2024-12-28 18:23:25 +0100

    switch to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
 import { createRoot } from "react-dom/client"
-import { BrowserRouter } from "react-router-dom"
+import { HashRouter } from "react-router-dom"
 import "./index.css"
 import App from "./pages/App"
 import { Overlay } from "./components/Overlay"
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/portfolio">
+  <HashRouter>
     <App />
     <Overlay
       email="mail@christianhohenbild.de"
       title="Creative Whatever"
       date="09/12/2024"
     />
-  </BrowserRouter>
+  </HashRouter>
 )
